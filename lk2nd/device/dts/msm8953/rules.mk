b85ba4a495f45024f13c43f42f42f906f9cb6d55
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-10 14:04:12 +0800

    dts: msm8953: Add Xiaomi UTER
    Change-Id: I8ed42188ff7b542b6994562a11c3f80ee88930bc

--- lk2nd/device/dts/msm8953/rules.mk
+++ lk2nd/device/dts/msm8953/rules.mk
@@ -17,6 +17,7 @@ ADTBS += \
 	$(LOCAL_DIR)/msm8953-xiaomi-daisy.dtb \
 	$(LOCAL_DIR)/msm8953-xiaomi-markw.dtb \
 	$(LOCAL_DIR)/msm8953-xiaomi-oxygen.dtb \
+	$(LOCAL_DIR)/msm8953-xiaomi-uter.dtb \
 	$(LOCAL_DIR)/msm8953-xiaomi-vince.dtb \
 	$(LOCAL_DIR)/sdm450-motorola-ali.dtb \
 	$(LOCAL_DIR)/sdm450-samsung-r04.dtb \
